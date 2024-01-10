d35c429a90c65a1d101513a2806dfb8d78570c4d
xwsjjctz <2251658569@qq.com>
2024-01-10 22:29:22 +0800

    使用ffmpy替换使用os库执行ffmpeg命令

--- LyricsAdapt.py
+++ LyricsAdapt.py
@@ -1,10 +1,8 @@
 from audio_processing import AudioProcessing
 
-with open("Mine - Phoebe Ryan.lrc", 'r') as f:
-    lrc = f.read()
-    music = AudioProcessing("『 叹 』又土又仙又涩，国风越南神曲翻唱_兰音.flac", 
-                            "叹", 
-                            "兰音Reine", 
-                            lrc, 
-                            "兰音Reine.jpeg")
-    music.metadata_processing()
+music = AudioProcessing("『 叹 』又土又仙又涩，国风越南神曲翻唱_兰音.flac", 
+                        "叹", 
+                        "兰音Reine", 
+                        "Mine - Phoebe Ryan.lrc", 
+                        "兰音Reine.jpeg")
+music.metadata_processing()
--- audio_processing.py
+++ audio_processing.py
@@ -1,5 +1,6 @@
 import os
 from mutagen.flac import FLAC
+from ffmpy import FFmpeg
 
 class AudioProcessing():
 
@@ -12,15 +13,22 @@ class AudioProcessing():
 
     def metadata_processing(self):
         temp = f"cover_{self.audio}"
-        os.system(f'''ffmpeg -i "{self.audio}" -i "{self.cover}" \
-                  -map 0:a -map 1 -codec copy \
-                  -metadata:s:v title="Album cover" \
-                  -metadata:s:v comment="Cover (front)" \
-                  -disposition:v attached_pic "{temp}" -v quiet''')
+        ff = FFmpeg(
+            inputs={self.audio: None, 
+                    self.cover: None}, 
+            outputs={temp: [
+                '-map', '0:a', '-map', '1', '-codec', 'copy', 
+                '-metadata:s:v', 'title=Album cover', 
+                '-metadata:s:v', 'comment=Cover (front)', 
+                '-disposition:v', 'attached_pic', 
+                '-v', 'quiet', '-y']})
+        ff.run()
         file = FLAC(temp)
         file["TITLE"] = self.title
         file["ARTIST"] = self.artist
-        file["LYRICS"] = self.lyrics
-        file.save()
+        with open(self.lyrics, 'r') as f:
+            lrc = f.read()
+            file["LYRICS"] = lrc
+            file.save()
         os.remove(self.audio)
         os.rename(temp, self.audio)
